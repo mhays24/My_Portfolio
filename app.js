146735b808eb04be1ad8a378a8d49002fb71b77c
Matthew Hays <matthewhays419@gmail.com>
2024-01-30 23:39:52 -0600

    January update - page redesign

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const logos = document.querySelectorAll('.skill_logos');
+    var skillLogos = document.querySelectorAll('.python-skill-logo, .java-script-skill-logo, .html-skill-logo, .css-skill-logo, .git-skill-logo, .github-skill-logo, .sql-skill-logo, .java-skill-logo, .django-skill-logo, .spring-skill-logo');
 
-    logos.forEach(logo => {
+    skillLogos.forEach(function (logo) {
         logo.addEventListener('click', function () {
-            alert(`You clicked on ${this.alt} logo!`);
+            var imageName = logo.getAttribute('data-name');
+            alert('You clicked on the ' + imageName + ' logo.');
         });
     });
 });
+
